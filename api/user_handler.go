ffaf0a921b06ec0aebf6b7673f336450b5813044
Amir <amirdp8102@gmail.com>
2024-05-26 22:08:14 +0330

    implement get user with mongo

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,11 +1,24 @@
 package api
 
 import (
+	"context"
+
 	"github.com/gofiber/fiber/v2"
+	"hotel.com/db"
 	"hotel.com/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	user := types.User{
 		FirstName: "TestFirst",
 		LastName:  "TestLast",
@@ -13,6 +26,16 @@ func HandleGetUsers(c *fiber.Ctx) error {
 	return c.JSON([]types.User{user})
 }
 
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("Single Useer")
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := h.userStore.GetUserByID(ctx, id)
+
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(user)
 }
--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"hotel.com/api"
+	"hotel.com/db"
 )
 
+const dburi = "mongodb://localhost:27017"
+
+var app = fiber.New(fiber.Config{
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		return ctx.JSON(map[string]string{"error": err.Error()})
+	},
+})
+
 func main() {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
 	address := flag.String("serverPort", ":5000", "")
 	flag.Parse()
 
-	app := fiber.New()
+	app := fiber.New(app.Config())
 
 	apiV1 := app.Group("api/v1")
 	apiV1.Get("/", handleHome)
 
-	apiV1.Get("/users", api.HandleGetUsers)
-	apiV1.Get("/users/:id", api.HandleGetUser)
+	apiV1.Get("/users", userHandler.HandleGetUsers)
+	apiV1.Get("/users/:id", userHandler.HandleGetUser)
 
 	app.Listen(*address)
 }
 
 func handleHome(c *fiber.Ctx) error {
-	
+
 	return c.JSON(map[string]string{"msg": "working"})
 }
