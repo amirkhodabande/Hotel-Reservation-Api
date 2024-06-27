89031a01b64b73bcd603cc240323ba08d902bacc
Amir <amirdp8102@gmail.com>
2024-06-27 12:47:07 +0330

    add 404 status

--- app/app.go
+++ app/app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
 	"hotel.com/db"
 	"hotel.com/routes"
 )
@@ -10,6 +13,10 @@ var app = fiber.New(fiber.Config{
 	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
 		if err.Error() == "unauthorized" || err.Error() == "invalid credentials" {
 			ctx.Status(fiber.StatusUnauthorized)
+		}
+
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			ctx.Status(fiber.StatusNotFound)
 		}
 
 		return ctx.JSON(map[string]string{"error": err.Error()})
